cd3a97f6a5d66be531ab571a020eceb8b9ef48cb
whopriyam <priyambasu16@gmail.com>
2020-03-21 14:08:04 +0530

    Add existing file

---
+++ wine_quality.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression 
+from sklearn import metrics 
+import matplotlib.pyplot as plt 
+import numpy as np 
+import seaborn as sns
